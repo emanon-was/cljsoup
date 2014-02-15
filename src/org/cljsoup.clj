9d57c2460518484370da6f1c53a1e0849b2a7f38
emanon-was <nicosm000@gmail.com>
2014-02-15 13:34:57 +0900

    なんかそれっぽくなってきた

--- src/org/cljsoup.clj
+++ src/org/cljsoup.clj
@@ -1,49 +1,89 @@
 (ns org.cljsoup
   (:gen-class)
   (:require [clojure.java.io :as io]
-            [org.cljsoup.protocol :as protocol]
-            [org.cljsoup.inherit  :as inherit]
             [hiccup.core :as hiccup])
   (:import [org.jsoup Jsoup]
-           [org.jsoup.nodes Attribute Attributes Document Element TextNode Node]
-           [org.jsoup.parser Parser]
-           [org.jsoup.select Selector]))
+           [org.jsoup.parser Parser]))
 
-(inherit/inherit-ns 'hiccup.core)
 
-(defn file-resource [path]
+(defn file-resource [#^String path]
   (slurp (str (System/getProperty "user.dir") "/" path)))
 
-;;(defprotocol Transform [selector alter])
-
-(defn parse [html]
+(defn parse [#^String html]
   (Jsoup/parse html "String" (Parser/xmlParser)))
 
-(defn clone [document]
+(defn html-expand [#^Element document]
+  (.toString document))
+
+(defn clone [#^Element document]
   (.clone document))
 
-(defn select [selector document]
+(defn select [#^Element document #^String selector]
   (.select document selector))
-    
-(defn attr
-  ([attribute] #(.attr % attribute))
-  ([attribute value] #(.attr % attribute value)))
+
+(defn selection* [#^Element document selector-and-method]
+  (let [document (clone document)
+        selector (first selector-and-method)
+        method   (second selector-and-method)
+        elements (select document selector)]
+    (if (nil? method)
+      elements
+      (map method elements))))
+
+(defn selection [#^Element document selector-and-method & more]
+  (if (empty? more)
+    (selection* document selector-and-method)
+    (map
+     #(selection* document %)
+     (cons selector-and-method more))))
+
+(defn attr [#^String attribute]
+  #(.attr % attribute))
 
 (defn transform* [document selector-and-method]
   (let [document (clone document)
         selector (first selector-and-method)
         method   (second selector-and-method)]
-    (do
-      (method (select selector document))
-      document)))
+    (method (select document selector))
+    document))
 
 (defn transform [document & selector-and-method]
-  (println selector-and-method)
-  (if selector-and-method
+  (if (empty? selector-and-method)
+    document
     (recur (transform* document (first selector-and-method))
-           (rest selector-and-method))
-    document))
+           (rest selector-and-method))))
+
+(defn add-class [class-name]
+  #(.addClass % class-name))
+
+(defn after [html]
+  #(.after % html))
+
+(defn before [html]
+  #(.before % html))
+
+(defn attr [attribute value]
+  #(.attr % attribute value))
+
   
 (def html (parse (file-resource "resources/test.html")))
 
 
+
+
+(defn- inherit-var [var]
+  (eval
+   (let [meta-data (meta var)
+         name      (meta-data :name)
+         macro     (meta-data :macro)
+         dynamic   (meta-data :dynamic)]
+     `(def ~(with-meta name
+              (assoc (meta name)
+                :macro   macro
+                :dynamic dynamic)) ~var))))
+
+(defn- inherit-ns [ns]
+  (let [vars (vals (ns-publics ns))]
+    (doseq [v vars] (inherit-var v))))
+
+(inherit/inherit-ns 'hiccup.core)
