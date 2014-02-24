ae6fcb59246cfd941f8b512f847c6476ff68022b
kawasaki <d-kawasaki@debian>
2014-02-24 19:22:57 +0900

    そろそろ名前変えようかな…

--- src/org/cljsoup.clj
+++ src/org/cljsoup.clj
@@ -1,27 +1,83 @@
 (ns org.cljsoup
   (:gen-class)
   (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [hiccup.core :as hiccup])
   (:import [org.jsoup Jsoup]
-           [org.jsoup.nodes Attribute Attributes Document Element TextNode]
+           [org.jsoup.nodes Document]
            [org.jsoup.parser Parser]))
 
-(defn file-resource [path]
+;;
+;; Copy hiccup
+;;
+
+(def ^:macro cljhtml #'hiccup/html)
+
+;;
+;; Utils
+;;
+
+(defn file-resource [#^String path]
   (slurp (str (System/getProperty "user.dir") "/" path)))
 
-(defn parse [html]
+(defn- ns-keyword []
+  (keyword (ns-name *ns*)))
+
+(defmacro ref-set! [name & body]
+  `(dosync (ref-set ~name ~@body)))
+
+;;
+;; Document methods
+;;
+
+(defn parse [#^String html]
   (Jsoup/parse html "String" (Parser/xmlParser)))
 
-(defn html-expand [document]
+(defn html-expand [#^Document document] ;;-> #^String
   (.toString document))
 
-(defn clone [document]
+(defn clone [#^Document document]
   (.clone document))
 
-(defn select [document selector]
+(defn select [#^Document document #^String selector]
   (.select document selector))
 
-(defn selection* [document selector-and-method]
+;;
+;; Element methods
+;;
+
+(defn add-class [#^String class-name]
+  #(.addClass % class-name))
+
+(defn after [#^String html]
+  #(.after % html))
+
+(defn before [#^String html]
+  #(.before % html))
+
+(defn append [#^String html]
+  #(.append % html))
+
+(defn prepend [#^String html]
+  #(.prepend % html))
+
+(defn attr
+  ([#^String attribute]
+     #(.attr % attribute))
+  ([#^String attribute #^String value]
+     #(.attr % attribute value)))
+
+(defn html
+  ([]
+     #(.html %))
+  ([#^String html]
+     #(.html % html)))
+
+;;
+;; Interface
+;;
+
+(defn- selection* [document selector-and-method]
   (let [document (clone document)
         selector (first selector-and-method)
         method   (second selector-and-method)
@@ -37,7 +93,7 @@
      #(selection* document %)
      (cons selector-and-method more))))
 
-(defn transform* [document selector-and-method]
+(defn- transform* [document selector-and-method]
   (let [document (clone document)
         selector (first selector-and-method)
         method   (second selector-and-method)]
@@ -50,39 +106,51 @@
     (recur (transform* document (first selector-and-method))
            (rest selector-and-method))))
 
-(defn add-class [class-name]
-  #(.addClass % class-name))
+;;
+;; Options
+;;
 
-(defn after [html]
-  #(.after % html))
+(def template-mode   (ref {}))
+(def template-prefix (ref {}))
+(def template-cache  (ref {}))
 
-(defn before [html]
-  #(.before % html))
+(def template-ns (ns-keyword))
+(ref-set! template-mode   (assoc @template-mode template-ns :development))
+(ref-set! template-prefix (assoc @template-prefix template-ns nil))
+(ref-set! template-cache  (assoc @template-cache template-ns nil))
+
+;;
+;; Privates
+;;
+
+(defn- use-mode []
+  (let [nskey (ns-keyword)]
+    (or (@template-mode nskey)
+        (@template-mode template-ns))))
+
+(defn- use-prefix []
+  (let [nskey (ns-keyword)]
+    (or (@template-prefix nskey)
+        (@template-prefix template-ns))))
+
+(defn- use-cache []
+  (let [nskey (ns-keyword)]
+    (or (@template-cache nskey)
+        (@template-cache template-ns))))
 
 
-(defn attr
-  ([attribute]
-     #(.attr % attribute))
-  ([attribute value]
-     #(.attr % attribute value)))
+;;
+;; Etc
+;;
 
-  
 (def abc (parse (file-resource "resources/test.html")))
 
+(defmacro time100 [& body]
+  (let [i (gensym)]
+    `(time (dotimes [~i 100]
+             ~@body))))
 
-(defn- inherit-var [var]
-  (eval
-   (let [meta-data (meta var)
-         name      (meta-data :name)
-         macro     (meta-data :macro)
-         dynamic   (meta-data :dynamic)]
-     `(def ~(with-meta name
-              (assoc (meta name)
-                :macro   macro
-                :dynamic dynamic)) ~var))))
-
-(defn- inherit-ns [ns]
-  (let [vars (vals (ns-publics ns))]
-    (doseq [v vars] (inherit-var v))))
-
-(inherit-ns 'hiccup.core)
+(defmacro time1000 [& body]
+  (let [i (gensym)]
+    `(time (dotimes [~i 1000]
+             ~@body))))
