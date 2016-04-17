1cef59b7e6d8ef6d2fcdc845488ffce7009c00c8
Daigo Kawasaki <kawasaki@fujisan.co.jp>
2016-04-17 22:31:26 +0900

    defstrictの定義をシンプルなものに変更

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.jsoup/jsoup "1.7.3"]
                  [hiccup "1.0.5"]])
--- src/cljsoup/macros.clj
+++ src/cljsoup/macros.clj
@@ -5,107 +5,47 @@
 ;; defstrict
 ;;
 
-(defrecord TypedArg [type arg])
+(defn types [& _]
+  (vec (map class _)))
 
-(let [common-type
-      {:Nil nil
-       :Function clojure.lang.Fn
-       :String java.lang.String
-       :Number java.lang.Number
-       :List clojure.lang.PersistentList
-       :Vector clojure.lang.PersistentVector
-       :HashMap clojure.lang.PersistentArrayMap
-       :HashSet clojure.lang.PersistentHashSet
-       :Symbol clojure.lang.Symbol
-       :Sequence clojure.lang.Sequential}]
+(def common-types
+  {:Nil      nil
+   :Function clojure.lang.Fn
+   :String   java.lang.String
+   :Number   java.lang.Number
+   :List     clojure.lang.PersistentList
+   :Vector   clojure.lang.PersistentVector
+   :HashMap  clojure.lang.PersistentArrayMap
+   :HashSet  clojure.lang.PersistentHashSet
+   :Symbol   clojure.lang.Symbol
+   :Sequence clojure.lang.Sequential})
 
-  (defn- build-typedarg [key sym]
-    (->TypedArg
-     (if (contains? common-type key)
-       (common-type key)
-       (symbol (name key)))
-     sym)))
+(defn- match-types
+  ([types-map] #(match-types types-map %))
+  ([types-map key]
+   (if (contains? types-map key)
+     (types-map key)
+     (symbol (name key)))))
 
+(defn- typed-args [_]
+  (let [args (partition 2 _)]
+    {:dispatch-val (vec (map first args))
+     :params       (vec (map second args))}))
 
-(defn- typed-args [args]
-  (loop [arg  (first  args)
-         more (rest args)
-         acc  nil]
-    (cond
-     (not arg) (reverse acc)
-     (empty? more) (reverse (cons arg acc))
-     :else (let [next (first more)
-                 key  (if (keyword? arg) arg nil)
-                 sym  (if (symbol? next) next nil)]
-             (cond
-              (not key) (recur next (rest more) (cons arg acc))
-              (= next '&) (recur next (rest more) acc)
-              sym (let [rest-more (rest more)]
-                    (recur (first rest-more) (rest rest-more)
-                           (cons (build-typedarg key sym) acc))))))))
-
-(defn- typed-arg? [arg]
-  (instance? TypedArg arg))
-
-(defn- signature [sig]
-  (let [base-args (typed-args (first sig))
-        target-args (filter typed-arg? base-args)
-        dispatch-args
-        (vec (map (fn [_] (if (typed-arg? _) (:arg _) _)) base-args))]
-    {:multi-args  dispatch-args
-     :multi-body  (vec (map (fn [target] `(class ~(:arg target))) target-args))
-     :method-vals (vec (map #(:type %) target-args))
-     :method-args dispatch-args
-     :method-body (rest sig)}))
-
-(defn- multi-signature [sig]
-  `(~(:multi-args sig)
-    ~(:multi-body sig)))
-
-(defn- method-signature [sig]
-  `(~(:method-vals sig)
-    ~(:method-args sig)
-    ~@(:method-body sig)))
-
-
-(defn- signatures [sigs]
-  (map signature sigs))
-
-(defn- multi-signatures [sigs]
-  (map (comp multi-signature first)
-       (vals (group-by #(count (:multi-args %)) sigs))))
-
-(defn- method-signatures [sigs]
-  (map method-signature sigs))
+(defn- method-signature [_]
+  (let [args (typed-args (first _))]
+    `(~(vec (map (match-types common-types) (:dispatch-val args)))
+      ~(:params args)
+      ~@(rest _))))
 
 (defmacro defstrict* [name & sigs]
-  (let [sigs (signatures sigs)
-        multi-sigs (multi-signatures sigs)
-        method-sigs (method-signatures sigs)]
+  (let [method-sigs (map method-signature sigs)]
     `(do
        (def ~name nil)
-       (defmulti ~name (fn ~@multi-sigs))
+       (defmulti ~name types)
        ~@(map (fn [s] `(defmethod ~name ~@s)) method-sigs))))
 
 (defmacro defstrict [name & sigs]
   (if (vector? (first sigs))
     `(defstrict* ~name ~sigs)
     `(defstrict* ~name ~@sigs)))
-
-;;
-;; ns-inheritance, ns-uninheritance
-;;
-
-(defmacro ns-inheritance [ns]
-  (let [publics (ns-publics ns)
-        syms (keys publics)]
-    `(do ~@(map (fn [sym] `(def ~sym ~(publics sym))) syms))))
-
-(defmacro ns-uninheritance [ns]
-  (let [syms (keys (ns-publics ns))]
-    `(do ~@(map (fn [s] `(ns-unmap '~(ns-name *ns*) '~s))
-                (filter #(= (ns-resolve (find-ns ns) %)
-                            (let [r (ns-resolve *ns* %)]
-                              (if r (var-get r) nil)))
-                        syms)))))
-
